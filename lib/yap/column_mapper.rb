5cf4a734e34725ae881330c9d1d986490b437c1e
Finn Glöe <login@amshaegar.de>
2016-07-16 12:33:21 +0000

    changed behaviour of Lint/EndAlignment cop

--- lib/yap/column_mapper.rb
+++ lib/yap/column_mapper.rb
@@ -2,10 +2,13 @@ module ColumnMapper
   private
 
   def map_column(name)
-    column_names.include?(name) ? name : if respond_to? :map_name_to_column
+    if column_names.include?(name)
+      name
+    elsif respond_to? :map_name_to_column
       map_name_to_column(name)
     else
-      warn "#{self.name} does not implement map_name_to_column. If you do not need column mapping set disable_warnings=true" unless Yap::DEFAULTS.disable_warnings
+      warn "#{self.name} does not implement map_name_to_column. If you do not need column mapping set " \
+          'disable_warnings=true' unless Yap::DEFAULTS.disable_warnings
       nil
     end
   end
