5eb727ee23bcdccece247e6423f06c37f659fdba
Finn Glöe <finn.gloee@1und1.de>
2017-02-17 10:50:58 +0100

    fixed rubocop offenses, reduced complexity

--- lib/yap/column_mapper.rb
+++ lib/yap/column_mapper.rb
@@ -1,23 +1,26 @@
 module ColumnMapper
   attr_reader :_api_aliases
   def api_aliases(aliases = nil, &block)
-    raise "Aliases already defined. Make sure to invoke #{__method__.to_s} only once." if @_api_aliases.present?
+    raise "Aliases already defined. Make sure to invoke #{__method__} only once." if defined? @_api_aliases
 
-    if aliases.present?
-      unless aliases.is_a? Hash
-        raise ArgumentError, "Expected first argument to be of type Hash, got #{aliases.class.name}."
-      end
-
-      @_api_aliases = aliases.symbolize_keys
-    end
+    api_aliases_hash(aliases) if aliases.present?
 
     if block_given?
-      if aliases.present?
-        raise ArgumentError, 'Only one of the following allowed: Hash of aliases or block.'
-      end
-
+      raise ArgumentError, 'Only one of the following allowed: Hash of aliases or block.' if aliases.present?
       @_api_aliases = block
     end
+
+    nil
+  end
+
+  def api_aliases_hash(aliases)
+    unless aliases.is_a? Hash
+      raise ArgumentError, "Expected first argument to be a Hash, got #{aliases.class.name}."
+    end
+
+    @_api_aliases = aliases.symbolize_keys
+
+    nil
   end
 
   def map_column(name)
